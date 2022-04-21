04d68837a7837b979e3b8f76151e7f5aeb54e52f
E <e@pastel.codes>
2022-04-21 17:31:37 +0100

    Add initial files

---
+++ src/main/java/com/ep396/Webapps2022/ejb/UserService.java
@@ -0,0 +1,54 @@
+package com.ep396.Webapps2022.ejb;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.ep396.Webapps2022.entity.Currency;
+import com.ep396.Webapps2022.entity.SystemUserGroup;
+import com.ep396.Webapps2022.entity.SystemUser;
+
+/**
+ *
+ * @author parisis
+ */
+@Stateless
+public class UserService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public UserService() {
+    }
+
+    public void registerUser(String username, String userpassword, String name, String surname, Float currencyCount, Currency currencyType) {
+        try {
+            SystemUser sys_user;
+            SystemUserGroup sys_user_group;
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            String passwd = userpassword;
+            md.update(passwd.getBytes("UTF-8"));
+            byte[] digest = md.digest();
+            BigInteger bigInt = new BigInteger(1, digest);
+            String paswdToStoreInDB = bigInt.toString(16);
+
+            // apart from the default constructor which is required by JPA
+            // you need to also implement a constructor that will make the following code succeed
+            sys_user = new SystemUser(username, paswdToStoreInDB, name, surname, currencyCount, currencyType);
+            sys_user_group = new SystemUserGroup(username, "users");
+
+            em.persist(sys_user);
+            em.persist(sys_user_group);
+            
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/main/java/com/ep396/Webapps2022/entity/SystemUser.java
@@ -0,0 +1,159 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ep396.Webapps2022.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author blankie
+ */
+
+@Entity
+public class SystemUser implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    String username;
+
+    @NotNull
+    String password;
+
+    @NotNull
+    String name;
+
+    @NotNull
+    String surname;
+
+    @NotNull
+    Float currencyCount;
+
+    @NotNull
+    Currency currencyType;
+
+    public SystemUser() {}
+
+    public SystemUser(String username, String password, String name, String surname, Float currencyCount,
+            Currency currencyType) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = username;
+        this.currencyCount = currencyCount;
+        this.currencyType = currencyType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Float getCurrencyCount() {
+        return currencyCount;
+    }
+
+    public void setCurrencyCount(Float currencyCount) {
+        this.currencyCount = currencyCount;
+    }
+
+    public Currency getCurrencyType() {
+        return currencyType;
+    }
+
+    public void setCurrencyType(Currency currencyType) {
+        this.currencyType = currencyType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + Objects.hashCode(this.surname);
+        hash = 71 * hash + Objects.hashCode(this.username);
+        hash = 71 * hash + Objects.hashCode(this.password);
+        hash = 71 * hash + Objects.hashCode(this.currencyCount);
+        hash = 71 * hash + Objects.hashCode(this.currencyType);
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SystemUser other = (SystemUser) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.surname, other.surname)) {
+            return false;
+        }
+
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+
+        if (!Objects.equals(this.currencyCount, other.currencyCount)) {
+            return false;
+        }
+
+        if (!Objects.equals(this.currencyType, other.currencyType)) {
+            return false;
+        }
+
+        return Objects.equals(this.password, other.password);
+    }
+}
